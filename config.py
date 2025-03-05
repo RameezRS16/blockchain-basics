0fe56df504c2049923aabb5cf61965f3948e0077
Rameez Raja Soomro <rameezrajasoomro83@gmail.com>
2025-03-05 11:20:37 -0800

    Create config.py

---
+++ config.py
@@ -0,0 +1,7 @@
+# Flask App Configuration
+FLASK_DEBUG = True  # Debug mode enabled
+FLASK_PORT = 5000  # Default port for the API
+
+# Blockchain Configuration
+BLOCKCHAIN_DIFFICULTY = 2  # Adjust mining difficulty
+MINING_REWARD = 10  # Reward for mining a block
